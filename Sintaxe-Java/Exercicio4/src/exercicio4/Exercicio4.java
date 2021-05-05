2318da9e6bac123fe44185f455464e016c0d3def
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2021-05-05 04:26:12 -0300

    Add files via upload

---
+++ Sintaxe-Java/Exercicio4/src/exercicio4/Exercicio4.java
@@ -0,0 +1,14 @@
+package exercicio4;
+
+public class Exercicio4 {
+
+    public static void main(String[] args){
+
+        for(int i = 1; i <= 100; ++i){
+            if (i % 3 == 0)
+                System.out.println("O valor " + i + " é múltiplo de 3.");
+        }
+
+    }
+    
+}
