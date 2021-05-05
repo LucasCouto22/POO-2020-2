c715db10bc430649cb109f2a55c49c5ac29db995
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2021-05-05 04:23:08 -0300

    Add files via upload

---
+++ Sintaxe-Java/Exercicio3/src/exercicio3/Exercicio3.java
@@ -0,0 +1,18 @@
+package exercicio3;
+
+
+public class Exercicio3 {
+
+    public static void main(String[] args){
+        
+        int soma = 0;
+        
+        for(int i = 1; i <= 1000; ++i){
+            soma = soma + i;
+        }
+        
+        System.out.println("A soma dos valores somados de 1 a 999 é: " + soma);
+
+    }
+    
+}
