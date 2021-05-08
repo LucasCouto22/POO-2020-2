a15c79684746e54e2af939bf3b7219ee08607320
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2021-05-08 17:06:21 -0300

    Add files via upload

---
+++ Construtores-Pacotes-e-Encapsulamento/Exercicio4/src/exercicio4/Data.java
@@ -0,0 +1,22 @@
+package exercicio4;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Data {
+    LocalDate date;
+    public Data(int dia, int mes, int ano){
+        this.date = LocalDate.of(ano, mes, dia);
+    }
+    public Data(String mes, int dia, int ano){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy");       
+        CharSequence data = mes + "/" + String.valueOf(dia) + "/" + String.valueOf(ano);
+        this.date = LocalDate.parse(data, formatter);
+    }
+    public Data(int dia, int ano){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy");
+        CharSequence data = String.valueOf(dia) + "/" + String.valueOf(ano);
+        this.date = LocalDate.parse(data, formatter);
+    }
+    
+}
---
+++ Construtores-Pacotes-e-Encapsulamento/Exercicio4/src/exercicio4/Exercicio4.java
@@ -0,0 +1,12 @@
+package exercicio4;
+
+public class Exercicio4 {
+
+    public static void main(String[] args){
+    Data data = new Data("Agosto", 8, 2017);
+    Data data2 = new Data(14, 01, 2000);
+    System.out.println(data);
+    System.out.println(data2);
+    }
+    
+}
