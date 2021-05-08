71e115cb9a10ae0f8bd7f2c28a5f502843a8008d
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2021-05-08 17:19:57 -0300

    Add files via upload

---
+++ Atributos-e-métodos-estáticos-Herança-e-Polimorfismo-Classes-Genéricas-e-aninhadas-e-enumerações/Exercicio1e2/src/exercicio1e2/Exercicio1e2.java
@@ -0,0 +1,14 @@
+package exercicio1e2;
+
+public class Exercicio1e2 {
+
+    public static void main(String[] args) {
+        
+        int campoFutebol = 8250; //Metros quadrados 
+        System.out.println("Um campo de futebol em pés quadrados: " + ConversaoDeUnidadesDeArea.meter_sqt_to_feet_sqt(campoFutebol));
+        System.out.println("Um campo de futebol em acres: " + ConversaoDeUnidadesDeArea.meter_sqt_to_acres(campoFutebol));
+        System.out.println("Um campo de futebol em centimetros quadrados: " + ConversaoDeUnidadesDeArea.meter_sqt_to_centi_sqt(campoFutebol));
+ 
+    }
+    
+}
