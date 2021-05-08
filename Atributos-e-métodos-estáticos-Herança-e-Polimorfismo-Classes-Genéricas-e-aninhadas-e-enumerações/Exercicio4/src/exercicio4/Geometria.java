80a5a3310e40e035a553ad08f073e7234536a289
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2021-05-08 17:20:53 -0300

    Add files via upload

---
+++ Atributos-e-métodos-estáticos-Herança-e-Polimorfismo-Classes-Genéricas-e-aninhadas-e-enumerações/Exercicio4/src/exercicio4/Geometria.java
@@ -0,0 +1,10 @@
+package exercicio4;
+
+public class Geometria {
+    
+    float area;
+    public void area(){
+        
+    }
+    
+}
---
+++ Atributos-e-métodos-estáticos-Herança-e-Polimorfismo-Classes-Genéricas-e-aninhadas-e-enumerações/Exercicio4/src/exercicio4/Quadrado.java
@@ -0,0 +1,19 @@
+package exercicio4;
+
+public class Quadrado extends Geometria {
+    float lado;
+
+    public void Quadrado(){
+
+    }
+
+    public void Quadrado(float lado){
+        this.lado = lado;
+    }
+
+    public double area(float lado){
+        this.area = lado * lado;
+        return this.area;
+    }
+    
+}
